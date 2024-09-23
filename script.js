093b9ea99c1298f21b376a08b4e58cdae132c4e2
yashaasidi <inasidi45@gmail.com>
2024-09-23 13:28:04 +0300

    html project

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active'); // Toggle the active class
+});
+
